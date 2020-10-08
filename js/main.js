fa660483ffb8decafe850a655435d194cd3b7c93
Luis Quispe <luisqrja@gmail.com>
2020-10-07 23:27:40 -0500

    Funcionallity for add active state menu link

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@
   console.log("JS Connect!! :)");
   const $burgerButton = document.querySelector("#js_burger-button");
   const $header = document.querySelector("#js_header");
+  const $menuLinks = Array.from(document.querySelectorAll(".menu-link"));
+  //Event click for each links of menu
+  $menuLinks.forEach((links) => {
+    links.addEventListener("click", (e) => {
+      if ($menuLinks.className == "menu-link is-active") {
+        removeClassIsActive();
+      } else {
+        removeClassIsActive();
+        links.classList.add("is-active");
+      }
+    });
+  });
+  //Function for remove active state links menu
+  function removeClassIsActive() {
+    $menuLinks.forEach((element) => {
+      element.classList.remove("is-active");
+    });
+  }
   //Function header fixed
   window.addEventListener("scroll", () => {
     let y = window.scrollY;
