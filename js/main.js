c01f2c0985f80b8999a2add8515419f875922f0b
Luis Quispe <luisqrja@gmail.com>
2020-10-07 23:09:10 -0500

    Funcionallity menu mobile and header fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 (function init() {
   console.log("JS Connect!! :)");
+  const $burgerButton = document.querySelector("#js_burger-button");
+  const $header = document.querySelector("#js_header");
+  //Function header fixed
+  window.addEventListener("scroll", () => {
+    let y = window.scrollY;
+    if (y > 56) {
+      $header.classList.add("fixed");
+    } else {
+      $header.classList.remove("fixed");
+    }
+  });
+  //Function showHideMenu
+  function showHideMenu(e) {
+    e.preventDefault();
+    const $menu = document.querySelector("#js_menu");
+    $menu.classList.toggle("is-active");
+  }
+  //Event click for show and hide menu
+  $burgerButton.addEventListener("click", showHideMenu);
 })();
