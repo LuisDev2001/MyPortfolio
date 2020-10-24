35e494cd73b53c36a683128c276d161465c6d3e7
Luis Quispe <cuarto87@hotmail.com>
2020-10-24 00:01:49 -0500

    Update moda and use form data to prepare send information at service mail

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,8 @@ import "../css/main.css";
   const $modalConstruction = document.querySelector(".modal.construction");
   const $btnModalOk = document.querySelector("#js_btn-ok");
   const $modalOpacity = document.querySelector(".modal-opacity");
-
+  //Form DOM 
+  const $form = document.querySelector("#js_form-data");
   /*
    * When a user click en send email appear modal of construction
    */
@@ -91,7 +92,7 @@ import "../css/main.css";
   /*
    * Function for remove active state links menu
    */
-  function removeClassIsActive() {
+  const removeClassIsActive = () => {
     $menuLinks.forEach((element) => {
       element.classList.remove("is-active");
     });
@@ -100,7 +101,7 @@ import "../css/main.css";
   /*
    * Function to put class 'fixed'
    */
-  function headerFixed() {
+  const headerFixed = () => {
     let y = window.scrollY;
     y > 56 ? $header.classList.add("fixed") : $header.classList.remove("fixed");
   }
@@ -108,7 +109,7 @@ import "../css/main.css";
   /*
    *Function showHideMenu
    */
-  function showHideMenu(e) {
+  const showHideMenu = (e) => {
     e.preventDefault();
     $menu.classList.toggle("is-active");
   }
@@ -117,13 +118,14 @@ import "../css/main.css";
    * Function returnTop
    * Execute function in event
    */
-  function returnTop() {
+  const returnTop = () => {
     window.scroll({
       top: `${100}%`,
       behavior: "smooth",
     });
     removeClassIsActive();
   }
+
   window.addEventListener("scroll", () => {
     let y = window.scrollY;
     y > 550
@@ -162,4 +164,52 @@ import "../css/main.css";
    * Executed function renderInformationPortfolio
    */
   renderInformationPortfolio();
+
+  /**
+   * Use Form Data to get information to send a service email in php
+   */
+  $form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = new FormData($form);
+    const name = data.get("name");
+    const email = data.get("email");
+    const phone = data.get("phone");
+    const message = data.get("message");
+
+    //Create object with form data
+    createObject(name, email, phone, message);
+    //Clean inputs
+    cleanInputs();
+    /**
+      * In this section add to code for put a loader or something for async function
+      */
+  });
+
+  /**
+   * Create this function to create a object to send service
+   * @param {*} name 
+   * @param {*} email 
+   * @param {*} phone 
+   * @param {*} message 
+   */
+  const createObject = (name, email, phone, message) => {
+    let ObjectToSendService = {
+      name: name,
+      email: email,
+      phone: phone,
+      message: message,
+    };
+    console.log(ObjectToSendService);
+    return ObjectToSendService;
+  }
+
+  /**
+   * Function to clean input of my form
+   */
+  const cleanInputs = () => {
+    const $inputs = document.querySelectorAll("input");
+    const $textArea = document.querySelector("textarea");
+    $inputs.forEach((input) => input.value = "")
+    $textArea.value = "";
+  }
 })();
